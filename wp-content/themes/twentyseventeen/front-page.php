97a96294c2a4e556c7a36cf7cd4f7bfbbeeb7103
peterhoejlundandersen <hilsenit@gmail.com>
2018-01-04 17:33:19 +0100

    typing numbers in without validation is working

--- wp-content/themes/twentyseventeen/front-page.php
+++ wp-content/themes/twentyseventeen/front-page.php
@@ -22,9 +22,10 @@ get_header(); ?>
 
 <div class="price-cat-btn-wrapper">
   <?php
+  $index_for_cat = 0;
   if( have_rows('price_cat', 'option') ):
     while ( have_rows('price_cat', 'option') ) : the_row(); ?>
-      <button class="price-cat-btn" id="<?php the_sub_field('titel'); ?>"
+      <button class="price-cat-btn <?php echo ($index_for_cat == 0) ? 'active' : '' ?>" id="<?php the_sub_field('titel'); ?>"
        data-a="<?php the_sub_field('a'); ?>"
        data-b="<?php the_sub_field('b'); ?>"
        data-c="<?php the_sub_field('c'); ?>"
@@ -32,10 +33,27 @@ get_header(); ?>
           <?php the_sub_field('titel'); ?>
       </button>
   <?php 
+    $index_for_cat++;
     endwhile;
   endif; ?>
 </div>
-
+<hr>
+<input id="ordTaeller" type="text" placeholder="Indtast antal ord.." /> 
+<div class="price-cat-discount-wrapper">
+  <?php
+  $index = 0;
+  if( have_rows('deadlines', 'option') ):
+    while ( have_rows('deadlines', 'option') ) : the_row(); ?>
+      <button class="price-cat-deadline-btn <?php echo ($index === 0) ? 'active' : '' ?>"
+       data-discount="<?php the_sub_field('rabat'); ?>">
+          <?php the_sub_field('periode'); ?>
+      </button>
+  <?php 
+    $index++;
+    endwhile;
+  endif; ?>
+</div>
+<h2 id="priceCatPrice"></h2>
 <script>
 
 function getData(id) {
@@ -45,6 +63,7 @@ function getData(id) {
   d = Number($("#" + id).data('d'));
   return { a: a, b: b, c: c, d: d }
 }
+
 function PriceCat(data) {
   this.a = data.a;
   this.b = data.b;
@@ -54,15 +73,45 @@ function PriceCat(data) {
     return (Math.round(number_of_words*this.a) - Math.pow(number_of_words*this.b, this.c)+1)+this.d;
   }
   this.price_with_deadline = function(word_count, discount) {
-    let discount_int = Number(discount) * 0.01; // For at procent skal blive et decimaltal: 10 * 0,01 = 0,1  
-    return (this.price(word_count) * (1 - discount_int));
+    if (word_count === 0) {
+      return 0;
+    } else {
+      let discount_int = Number(discount) * 0.01; // For at procent skal blive et decimaltal: 10 * 0,01 = 0,1  
+      return (this.price(word_count) * (1 - discount_int));
+    }
   }
 }
 
+function changePrice(cons_name, data) {
+  $("#priceCatPrice").html(window[cons_name].price_with_deadline(data.word_count, data.discount)); 
+}
+
+function getDiscountAndWordCount() { 
+  let word_count = Number($("#ordTaeller").val()) || 0; // Returnere 0, hvis der ikke er indtastet noget endnu.
+  let discount = Number($(".price-cat-discount-wrapper .active").data('discount'));
+  return { word_count: word_count, discount: discount }
+}
+
 $(document).ready(function() {
+
   $(".price-cat-btn").each(function(i, price_cat) {
-    window[price_cat.id] = new PriceCat(getData(price_cat.id)); 
+    window[price_cat.id] = new PriceCat(getData(price_cat.id));
+    if (i === 0) { changePrice(price_cat.id, getDiscountAndWordCount()); } // Sæt prisen ved første priskategori.
   });
+
+  $(".price-cat-btn").click(function() { // Skifter pris kategori via klik
+    changePrice(this.id, getDiscountAndWordCount());
+  });
+
+  $(".price-deadline-btn").click(function() { // Skifter deadline via klik
+    $("#ordTaeller").val();
+  });
+
+  $("#ordTaeller").on("input", function() { // Når der bliver tastet noget nyt ind i antal-ord feltet.
+    let active_price_cat_id = $(".price-cat-btn-wrapper .active").attr('id');
+    changePrice(active_price_cat_id, getDiscountAndWordCount());
+  });
+
 });
 
 </script>
